ee8e39bcf0fe5f889422e75c6a319deb9d82655e
Raj Madhuram <rmadhuram@yahoo.com>
2022-12-25 13:00:19 +0530

    tree drawing

--- christmas-tree/tree.js
+++ christmas-tree/tree.js
@@ -1,27 +1,41 @@
+
 var c = document.getElementById("drawing");
-
-
 var ctx = c.getContext("2d");
-ctx.strokeStyle = "#004400";
-let nDiv = 8
+ctx.strokeStyle = "#188c37";
+let nDiv = 7
+
+function rnd(n) {
+  return Math.random()*2*n - n
+  //return 0
+}
 
 function drawTree(x, y, len, angle, n) {
   if (n == 0) return
 
+ 
   let x1 = x + len * Math.cos(angle * Math.PI / 180)
   let y1 = y + len * Math.sin(angle * Math.PI / 180)
   ctx.moveTo(x, y);
   ctx.lineTo(x1, y1);
 
-  for (let i = 2; i <= nDiv; i++) {
+  for (let i = 1; i <= nDiv; i++) {
     let l = i * len / nDiv
     x1 = x + l * Math.cos(angle * Math.PI / 180)
     y1 = y + l * Math.sin(angle * Math.PI / 180)
-    drawTree(x1, y1, 0.5 * (len - l + len/nDiv), angle + 120, n-1)
-    drawTree(x1, y1, 0.5 * (len - l + len/nDiv), angle - 120, n-1)
+    drawTree(x1, y1, 0.7 * (len - l + len/nDiv) + rnd(5), angle + 130 + rnd(5), n-1)
+    drawTree(x1, y1, 0.7 * (len - l + len/nDiv) + rnd(5), angle - 130 + rnd(5), n-1)
   }
+
+  //ctx.lineWidth = (n == 4) ? 2 : 1;
 
   ctx.stroke()
 }
 
-drawTree(500, 350, 300, 270, 4);
+ctx.fillStyle = "#735019";
+ctx.beginPath();
+ctx.moveTo(450, 750);
+ctx.lineTo(550, 750);
+ctx.lineTo(500, 50);
+ctx.fill();
+
+drawTree(500, 600, 500, 270, 4);
